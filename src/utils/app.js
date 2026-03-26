edceb8edbbcf665169323812e1232e331c3952eb
Md Saddam <mdsonu7860@outlook.com>
2026-03-26 19:51:50 +0530

    Add Health Status and Manual sync endpoint

--- src/utils/app.js
+++ src/utils/app.js
@@ -1,8 +1,25 @@
 import express from "express";
 
+import { getCompanies } from "../services/orderwise.service.js";
+
 const app = express();
 
 app.use(express.json());
 
-export default app;
+app.get("/", (req, res) => {
+  res.send("Server is Healthy");
+});
+app.get("/health", (req, res) => {
+  res.send("Server is Healthy");
+});
 
+app.post("/start-sync", (req, res) => {
+  try {
+    res.send("Sync Started Manually");
+    getCompanies();
+  } catch (error) {
+    console.error("Error starting sync:", error);
+  }
+});
+
+export default app;
